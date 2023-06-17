c8b0925cb61869816067c522ea297283965bac7b
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2023-06-17 12:01:48 +0100

    Remove other two example widgets.

--- src/cavendish_particle_tracks/_widget.py
+++ src/cavendish_particle_tracks/_widget.py
@@ -6,13 +6,9 @@ see: https://napari.org/stable/plugins/guides.html?#widgets
 
 Replace code below according to your needs.
 """
-from typing import TYPE_CHECKING
 
-from magicgui import magic_factory
+import napari
 from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
-
-if TYPE_CHECKING:
-    import napari
 
 
 class ExampleQWidget(QWidget):
@@ -20,27 +16,17 @@ class ExampleQWidget(QWidget):
     # in one of two ways:
     # 1. use a parameter called `napari_viewer`, as done here
     # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
+    def __init__(self, napari_viewer: napari.viewer.Viewer):
         super().__init__()
         self.viewer = napari_viewer
 
         btn = QPushButton("Click me!")
         btn.clicked.connect(self._on_click)
 
+        # layer.world_to_data(viewer.cursor.position)
+
         self.setLayout(QHBoxLayout())
         self.layout().addWidget(btn)
 
     def _on_click(self):
         print("napari has", len(self.viewer.layers), "layers")
-
-
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
-
-
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
