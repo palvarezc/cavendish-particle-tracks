dc1d3eaebb7d58dc43b7b37655658a96297c64b4
Paula Alvarez <paula.alvarez@cern.ch>
2023-08-02 15:11:28 +0100

    Add test for length

--- tests/test_radius_calculation.py
+++ tests/test_radius_calculation.py
@@ -1,5 +1,7 @@
+from math import sqrt
+
 import pytest
-from cavendish_particle_tracks._calculate import radius
+from cavendish_particle_tracks._calculate import length, radius
 
 
 @pytest.mark.parametrize(
@@ -12,3 +14,15 @@ from cavendish_particle_tracks._calculate import radius
 )
 def test_calculate_radius(a, b, c, R):
     assert radius(a, b, c) == pytest.approx(R, rel=1e-3)
+
+
+@pytest.mark.parametrize(
+    "a, b, L",
+    [
+        ((0, 1), (1, 0), sqrt(2)),
+        ((-3, 3), (-3, 2), 1),
+        ((1, 1), (3, 4), sqrt(4 + 9)),
+    ],
+)
+def test_calculate_length(a, b, L):
+    assert length(a, b) == pytest.approx(L, rel=1e-3)
