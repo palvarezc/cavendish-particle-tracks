8385936bfb6eca4132543df3a5ca9c68de4ccbe0
Joe Garvey <jmg237@cam.ac.uk>
2024-08-02 14:15:01 +0100

    removed exit on error clause

--- launch_debug.py
+++ launch_debug.py
@@ -5,9 +5,7 @@ from napari import Viewer, run
 
 viewer = Viewer()
 dock_widget, plugin_widget = viewer.window.add_plugin_dock_widget(
-    "cavendish-particle-tracks",
-    "Cavendish Particle Tracks Analysis",
-    NAPARI_EXIT_ON_ERROR=1,
+    "cavendish-particle-tracks", "Cavendish Particle Tracks Analysis"
 )
 # Optional steps to setup your plugin to a state of failure
 # E.g. plugin_widget.parameter_name.value = "some value"
