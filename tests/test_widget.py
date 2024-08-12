0338656324ac7fb538ee52a2bc43ce52da5096c1
Paula Alvarez <paula.alvarez@cern.ch>
2024-08-12 09:57:06 +0100

    Cleaning test dialog

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,16 +1,24 @@
+import time
+from random import random
 from typing import Callable
 
-from random import random
 import numpy as np
 import pytest
-import time
-from qtpy.QtCore import QTimer, Qt
-from qtpy.QtWidgets import QDialog, QFileDialog, QApplication, QPushButton, QDialogButtonBox #QFileDialog
-
 from cavendish_particle_tracks import ParticleTracksWidget
+from qtpy.QtCore import Qt, QTimer
+from qtpy.QtWidgets import (  # QFileDialog
+    QApplication,
+    QDialog,
+    QDialogButtonBox,
+)
 
 
-def get_dialog(dialog_trigger: Callable, dialog_action: Callable, time_out: int = 5, trigger_option: int = 1) -> QDialog:
+def get_dialog(
+    dialog_trigger: Callable,
+    dialog_action: Callable,
+    time_out: int = 5,
+    trigger_option: int = 1,
+) -> QDialog:
     """
     Returns the current dialog (active modal widget). If there is no
     dialog, it waits until one is created for a maximum of 5 seconds (by
@@ -32,13 +40,12 @@ def get_dialog(dialog_trigger: Callable, dialog_action: Callable, time_out: int 
 
         # Avoid errors when dialog is not created
         if dialog is not None:
-            # Hide dialog to avoid interrupting the tests execution
-            # It has the same effect as close()
+            # Perform action dialog_action()
+            # dialog.hide() and dialog.close() do not seem to work
             dialog_action(dialog)
-            # dialog.hide()
 
     # Create a thread to get the dialog instance and call dialog_creation trigger
-    QTimer.singleShot(1, dialog_creation)  
+    QTimer.singleShot(1, dialog_creation)
     dialog_trigger(trigger_option)
 
     # Wait for the dialog to be created or timeout
@@ -49,9 +56,7 @@ def get_dialog(dialog_trigger: Callable, dialog_action: Callable, time_out: int 
         dialog, QDialog
     ), f"No dialog was created after {time_out} seconds. Dialog type: {type(dialog)}"
 
-    return dialog    
-
-
+    return dialog
 
 
 def test_calculate_radius_ui(make_napari_viewer, capsys):
@@ -203,61 +208,30 @@ def test_calculate_length_fails_with_wrong_number_of_points(
     )
 
 
-
-
-
 @pytest.mark.parametrize(
     "image_folder",
-    ["/Users/paula/Desktop/CPT_data/view1",
-    #  "/Users/paula/Work/ParticleTracks/cavendish-particle-tracks/data_view1"
-     ],
+    [
+        "./tests/data",
+    ],
 )
 def test_load_data(make_napari_viewer, qtbot, image_folder):
     """Test loading of images in a folder as stack associated to a certain view"""
     viewer = make_napari_viewer()
     widget = ParticleTracksWidget(viewer)
 
-    # click "load data" and select "View1"
-    # widget.load.setCurrentIndex(1)
-
-    def exec_dialog(dialog):
+    def set_directory_and_close(dialog):
         qtbot.addWidget(dialog)
         dialog.setDirectory(image_folder)
-        # print("The dialog property is: ",dialog.DontUseNativeDialog)
-        # dialog.setOption(QFileDialog.DontUseNativeDialog)
-        # print("The dialog property is: ",dialog.DontUseNativeDialog)
-        # assert False, "I make it here"
-        # dialog.accept()
-        
-        buttonbox = dialog.findChild(QDialogButtonBox,"buttonBox")
-        # assert buttonbox, "No buttonbox found"
-        # if buttonbox: print("If found the buttonbox")
-
+        buttonbox = dialog.findChild(QDialogButtonBox, "buttonBox")
         openbutton = buttonbox.children()[1]
-        # assert openbutton, "No openbutton found"
         qtbot.mouseClick(openbutton, Qt.LeftButton, delay=1)
 
-        # # dialog.accept()
-        # buttons = dialog.findChild(QPushButton,"Open")
-        # qtbot.keyPress(buttons, Qt.Key_Enter)
-        # print("Dialog button children")
-        # dialog.hide()
-        # dialog.selectFile(image_folder)
+    # Open and retreive file dialog
+    get_dialog(
+        widget.load.setCurrentIndex,
+        set_directory_and_close,
+        time_out=5,
+        trigger_option=1,
+    )
 
-    get_dialog(widget.load.setCurrentIndex, exec_dialog, time_out = 5, trigger_option = 1)
-
-#     >>> import tifffile
-# >>> import tifffile as tf
-# >>> import tifffile
-# KeyboardInterrupt
-# >>> import numpy as np
-# >>> numpy.random.randint(0, 255, (256, 256, 3), 'uint8')
-# Traceback (most recent call last):
-#   File "<stdin>", line 1, in <module>
-# NameError: name 'numpy' is not defined
-# >>> data = np.random.randint(0, 255, (256, 256, 3), 'uint8')
-# >>> tf.imwrite('data_view1/data1.tiff', data, photometric='rgb')
-# >>> data = np.random.randint(0, 255, (256, 256, 3), 'uint8')
-# >>> tf.imwrite('data_view1/data2.tiff', data, photometric='rgb')
-# >>> 
-
+    print(viewer.layers)
