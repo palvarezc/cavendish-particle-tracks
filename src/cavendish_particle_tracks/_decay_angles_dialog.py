2d28d5125bd879fc3811ea71ffe7ab0b0cf28a73
Joe Garvey <jmg237@cam.ac.uk>
2024-08-28 20:46:31 +0100

    Partially working shape lock. Only updates when something else in the layer is changed.

--- src/cavendish_particle_tracks/_decay_angles_dialog.py
+++ src/cavendish_particle_tracks/_decay_angles_dialog.py
@@ -1,4 +1,6 @@
 import numpy as np
+from napari.layers import Shapes
+from napari.utils.events.event import Event
 from qtpy.QtWidgets import (
     QDialog,
     QDialogButtonBox,
@@ -14,32 +16,26 @@ from ._calculate import angle, track_parameters
 class DecayAnglesDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
-
         self.parent = parent
-
+        # region UI Setup
         self.setWindowTitle("Decay Angles")
-
         # text boxes for track parameters and results
         self.textboxes_slope = [QLabel(self) for _ in range(3)]
         self.textboxes_intercept = [QLabel(self) for _ in range(3)]
         self.textboxes_phi = [QLabel(self) for _ in range(2)]
-
         for textbox in (
             self.textboxes_slope
             + self.textboxes_intercept
             + self.textboxes_phi
         ):
             textbox.setMinimumWidth(200)
-
-        bss = QPushButton("Calculate")
-        bss.clicked.connect(self._on_click_calculate)
-
-        bap = QPushButton("Save to table")
-        bap.clicked.connect(self._on_click_save_to_table)
-
+        # buttons
+        btn_calculate = QPushButton("Calculate")
+        btn_calculate.clicked.connect(self._on_click_calculate)
+        btn_save = QPushButton("Save to table")
+        btn_save.clicked.connect(self._on_click_save_to_table)
         self.buttonBox = QDialogButtonBox(QDialogButtonBox.Cancel)
         self.buttonBox.clicked.connect(self.cancel)
-
         # layout
         self.setLayout(QGridLayout())
         self.layout().addWidget(
@@ -52,8 +48,7 @@ class DecayAnglesDialog(QDialog):
         ):
             self.layout().addWidget(widget, i % 3 + 1, i // 3)
 
-        self.layout().addWidget(bss, 4, 0, 1, 3)
-
+        self.layout().addWidget(btn_calculate, 4, 0, 1, 3)
         self.layout().addWidget(
             QLabel("Opening angles"),
             5,
@@ -66,23 +61,41 @@ class DecayAnglesDialog(QDialog):
             + self.textboxes_phi
         ):
             self.layout().addWidget(widget, i % 2 + 6, i // 2 + 1)
-        self.layout().addWidget(bap, 8, 0, 1, 3)
+        self.layout().addWidget(btn_save, 8, 0, 1, 3)
         self.layout().addWidget(self.buttonBox, 10, 0, 1, 3)
-
+        # endregion
         # Setup shapes layer
-        self.cal_layer = self._setup_decayangles_layer()
+        self.join_coordinates = [200, 300]
+        self.cal_layer: Shapes = self._setup_decayangles_layer()
 
         # Decay Angles related parameters
         self.phi_proton = 0.0
         self.phi_pion = 0.0
         self.alines = []
 
+        @self.cal_layer.events.data.connect
+        def enforce_points_coincident(event: Event) -> None:
+            # event.dataindicies says what shape was changed
+            # action says what was done to the shape
+            # data doesn't get set till user lets go, mouse drag might be better
+            match event.index:
+                case 0:
+                    self.cal_layer.data[1][0] = self.cal_layer.data[0][1]
+                    self.cal_layer.data[2][0] = self.cal_layer.data[0][1]
+                case 1:
+                    self.cal_layer.data[0][1] = self.cal_layer.data[1][0]
+                    self.cal_layer.data[2][0] = self.cal_layer.data[1][0]
+                case 2:
+                    self.cal_layer.data[0][1] = self.cal_layer.data[2][0]
+                    self.cal_layer.data[1][0] = self.cal_layer.data[2][0]
+            # self.cal_layer.refresh() for some reason calling layer refresh actually stops it refreshing
+
     def _setup_decayangles_layer(self):
         """Create a shapes layer and add three lines to measure the Lambda, p and pi tracks"""
 
-        lambda_line = np.array([[100, 100], [200, 300]])
-        proton_line = np.array([[200, 300], [300, 400]])
-        pion_line = np.array([[200, 300], [210, 400]])
+        lambda_line = np.array([[100, 100], self.join_coordinates])
+        proton_line = np.array([self.join_coordinates, [300, 400]])
+        pion_line = np.array([self.join_coordinates, [210, 400]])
 
         lines = [lambda_line, proton_line, pion_line]
 
