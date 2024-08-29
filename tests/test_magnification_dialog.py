f80071a5b810e0f4e771f5b2041d7d8ed9134c79
Paula Alvarez <paula.alvarez@cern.ch>
2024-08-29 16:49:04 +0100

    linter errors

--- tests/test_magnification_dialog.py
+++ tests/test_magnification_dialog.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+
 from cavendish_particle_tracks._analysis import Fiducial
 from cavendish_particle_tracks._calculate import CHAMBER_DEPTH as CD
 from cavendish_particle_tracks._calculate import FIDUCIAL_BACK as FB
