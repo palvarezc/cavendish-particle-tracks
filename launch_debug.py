9877f8e18a9d6f50821f58bee971d5a34a4551c8
Joe Garvey <jmg237@cam.ac.uk>
2024-09-02 15:08:00 +0100

    added the ability to bottom dock the window on launch

--- launch_debug.py
+++ launch_debug.py
@@ -6,6 +6,12 @@ viewer = Viewer()
 dock_widget, plugin_widget = viewer.window.add_plugin_dock_widget(
     "cavendish-particle-tracks", "Cavendish Particle Tracks Analysis"
 )
+
+# or if you want it to dock somewhere
+#dock_widget, plugin_widget = viewer.window.add_plugin_dock_widget("cavendish-particle-tracks", "Cavendish Particle Tracks Analysis")
+
+# Manually set the dock widget's position to the bottom
+#viewer.window.add_dock_widget(dock_widget, area='bottom')
 # Optional steps to setup your plugin to a state of failure
 # E.g. plugin_widget.parameter_name.value = "some value"
 # E.g. plugin_widget.button.click()
