c0817d7c3d4dccfb197dd81b6d091111c28d21e0
Paula Alvarez <paula.alvarez@cern.ch>
2024-09-12 00:48:25 +0100

    bug fix stereoshift: save to table

--- tests/test_stereoshift_dialog.py
+++ tests/test_stereoshift_dialog.py
@@ -106,3 +106,18 @@ def test_calculate_stereoshift_ui(
     assert cpt_widget.data[0].stereoshift == dlg.point_stereoshift
     # these names should be more consistent between different parts of the program
     assert cpt_widget.data[0].depth_cm == dlg.point_depth
+
+
+def test_stereoshift_save_to_table_fails_with_empty_table(cpt_widget, capsys):
+    """Test the expected failure modes: if I don't have any data in the table."""
+    # open the dialog
+    dlg = cpt_widget._on_click_stereoshift()
+
+    # click the save to table button
+    dlg._on_click_save_to_table()
+    captured = capsys.readouterr()
+
+    assert (
+        "ERROR: There are no particles in the table."
+        in captured.out
+    )
