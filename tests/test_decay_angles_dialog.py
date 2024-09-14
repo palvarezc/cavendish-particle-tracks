d4560b45add22bb15f7950926ffc859c41b896a1
Paula Alvarez Cartelle <paula.alvarez@cern.ch>
2024-09-14 19:05:34 +0100

    Update tests/test_decay_angles_dialog.py
    Co-authored-by: Sam Cunliffe <samcunliffe@users.noreply.github.com>

--- tests/test_decay_angles_dialog.py
+++ tests/test_decay_angles_dialog.py
@@ -13,9 +13,10 @@ def test_decay_angles_dialog(cpt_widget, qtbot: QtBot, click_twice: bool):
     """Smoke test for the DecayAnglesDialog."""
     cpt_widget.cb.setCurrentIndex(4)
 
+    # The user clicks the button (also test fringe case that they click the button again).
+    if click_twice:
+        cpt_widget._on_click_decay_angles()
     dialog = cpt_widget._on_click_decay_angles()
-    if double_click:
-        dialog = cpt_widget._on_click_decay_angles()
 
     qtbot.addWidget(dialog)
 
