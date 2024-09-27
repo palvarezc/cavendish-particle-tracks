fixing calculation and adding tests

src/cavendish_particle_tracks/_decay_angles_dialog.py
@@ -43,17 +43,20 @@ class DecayAnglesDialog(QDialog):
         self.layout().addWidget(
             QLabel("Track parameters (y = a x + b)"), 0, 0, 1, 2
         )
+        for i, widget in enumerate([QLabel("a"), QLabel("b")]):
+            self.layout().addWidget(widget, 1, i + 1)
+
         for i, widget in enumerate(
             [QLabel("Λ"), QLabel("p"), QLabel("π")]
             + self.textboxes_slope
             + self.textboxes_intercept
         ):
-            self.layout().addWidget(widget, i % 3 + 1, i // 3)
+            self.layout().addWidget(widget, i % 3 + 2, i // 3)
 
-        self.layout().addWidget(btn_calculate, 4, 0, 1, 3)
+        self.layout().addWidget(btn_calculate, 5, 0, 1, 3)
         self.layout().addWidget(
             QLabel("Opening angles"),
-            5,
+            6,
             0,
             1,
             3,
@@ -62,9 +65,9 @@ class DecayAnglesDialog(QDialog):
             [QLabel("ϕ_proton [rad]"), QLabel("ϕ_pion [rad]")]
             + self.textboxes_phi
         ):
-            self.layout().addWidget(widget, i % 2 + 6, i // 2 + 1)
-        self.layout().addWidget(btn_save, 8, 0, 1, 3)
-        self.layout().addWidget(self.buttonBox, 10, 0, 1, 3)
+            self.layout().addWidget(widget, i % 2 + 7, i // 2 + 1)
+        self.layout().addWidget(btn_save, 9, 0, 1, 3)
+        self.layout().addWidget(self.buttonBox, 11, 0, 1, 3)
         # endregion
         # Setup shapes layer
         self.join_coordinates = [200, 300]
@@ -123,6 +126,7 @@ class DecayAnglesDialog(QDialog):
 
         zoom_factor = self.parent.viewer.camera.zoom
 
+        # The first point for all tracks is the decay vertex
         # Scale offsets by the inverse of the zoom factor
         lambda_line = np.array(
             [
@@ -148,7 +152,7 @@ class DecayAnglesDialog(QDialog):
 
         text = {
             "string": ["Λ", "p", "π"],
-            "size": 14,
+            "size": 20,
             "color": colors,
             "translation": np.array([-30, 0]),
         }
@@ -167,11 +171,19 @@ class DecayAnglesDialog(QDialog):
     def _on_click_calculate(self) -> None:
         """When 'Calculate' button is clicked, calculate opening angles and populate table"""
 
-        # Compute tracks and angles
-        tracks = [track_parameters(line) for line in self.cal_layer.data]
+        # The Lambda travels towards the decay vertex, line needs to be reversed
+        lambda_line = self.cal_layer.data[0][::-1]
+        proton_line = self.cal_layer.data[1]
+        pion_line = self.cal_layer.data[2]
 
-        self.phi_proton = angle(self.cal_layer.data[0], self.cal_layer.data[1])
-        self.phi_pion = angle(self.cal_layer.data[0], self.cal_layer.data[2])
+        # Save the lines
+        self.alines = [lambda_line, proton_line, pion_line]
+
+        # Compute tracks and angles
+        tracks = [track_parameters(line) for line in self.alines]
+
+        self.phi_proton = angle(lambda_line, proton_line)
+        self.phi_pion = angle(lambda_line, pion_line)
 
         # # Populate the table
         for slope, intercept, track in zip(
@@ -183,9 +195,6 @@ class DecayAnglesDialog(QDialog):
         self.textboxes_phi[0].setText(str(self.phi_proton))
         self.textboxes_phi[1].setText(str(self.phi_pion))
 
-        # Save points
-        self.alines = self.cal_layer.data
-
     def _on_click_save_to_table(self) -> None:
         """When 'Save to table' button is clicked, propagate stereoshift and depth to main table"""
 
@@ -195,22 +204,10 @@ class DecayAnglesDialog(QDialog):
         except IndexError:
             show_error("There are no particles in the table.")
         else:
-            # self.parent.data[selected_row].spoints = self.alines
+            # self.parent.data[selected_row].alines = self.alines
             self.parent.data[selected_row].phi_proton = self.phi_proton
             self.parent.data[selected_row].phi_pion = self.phi_pion
 
-            # Propagate to parent table
-            # for i in range(2):
-            #     self.parent.table.setItem(
-            #         selected_row,
-            #         self.parent._get_table_column_index("sf" + str(i + 1)),
-            #         QTableWidgetItem(str(self.spoints[i])),
-            #     )
-            #     self.parent.table.setItem(
-            #         selected_row,
-            #         self.parent._get_table_column_index("sp" + str(i + 1)),
-            #         QTableWidgetItem(str(self.spoints[i + 2])),
-            #     )
             self.parent.table.setItem(
                 selected_row,
                 self.parent._get_table_column_index("phi_proton"),
@@ -224,8 +221,6 @@ class DecayAnglesDialog(QDialog):
 
     def reject(self) -> None:
         """On cancel remove the points_Stereoshift layer"""
-
-        # TODO: this is a problem, the layer still exists... not sure how to remove it
         self.parent.viewer.layers.select_previous()
         self.parent.viewer.layers.remove(self.cal_layer)
         self.parent.decay_angles_isopen = False