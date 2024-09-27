304901878febe55efd3e280324d9d1cafaf1d1f8
Paula Alvarez <paula.alvarez@cern.ch>
2024-09-27 16:11:36 +0100

    add test

--- tests/test_decay_angles_dialog.py
+++ tests/test_decay_angles_dialog.py
@@ -25,3 +25,24 @@ def test_decay_angles_dialog(cpt_widget, qtbot: QtBot, click_twice: bool):
         dialog.show()
     dialog.close()
     qtbot.waitSignals([dialog.rejected], timeout=5000)
+
+
+def test_decay_vertex_update(cpt_widget):
+
+    cpt_widget.particle_decays_menu.setCurrentIndex(4)
+    dialog = cpt_widget._on_click_decay_angles()
+
+    data = dialog.cal_layer.data
+
+    # Move the decay vertex
+    old_value = data[0][0][0]
+    data[0][0][0] = old_value + 50
+    dialog.cal_layer.data = (
+        data  # For some reason, this doesn't trigger the change event
+    )
+    dialog.cal_layer.events.data(action="changed", data_indices=(0,))
+
+    # Check that the decay vertex has been updated
+    assert dialog.cal_layer.data[0][0][0] == old_value + 50
+    assert (dialog.cal_layer.data[1][0] == data[0][0]).all()
+    assert (dialog.cal_layer.data[2][0] == data[0][0]).all()
