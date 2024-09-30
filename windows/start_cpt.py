20170ab7a725433dbbedb175d786524bba076718
Paula Alvarez Cartelle <paula.alvarez@cern.ch>
2024-09-30 22:09:56 +0100

    Update windows/start_cpt.py
    Co-authored-by: Sam Cunliffe <samcunliffe@users.noreply.github.com>

--- windows/start_cpt.py
+++ windows/start_cpt.py
@@ -1,4 +1,7 @@
-# Start napari with the cavendish-particle-tracks plugin docked at the bottom
+"""Start napari with the cavendish-particle-tracks plugin docked at the bottom
+
+This script is intended to start `napari` with the plugin for use in the Cavendish lab computers.
+"""
 from napari import Viewer, run
 
 from cavendish_particle_tracks import ParticleTracksWidget
